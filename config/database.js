f04f2c936f2c513d7d819f296f7957336c43f416
Kim Flores <61999639+Kim-Flores@users.noreply.github.com>
2020-10-09 11:14:13 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://crudtest:crud@cluster0.yy9ks.gcp.mongodb.net/leaf?retryWrites=true&w=majority'
+    'url' : ''
 
 // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
